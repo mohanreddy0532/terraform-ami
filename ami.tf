f6c2e8922911a31bfd88c2948243b3633681437c
mohan <mohanreddy0532@gmail.com>
2022-02-10 06:50:09 +0530

    tf ami

---
+++ ami.tf
@@ -0,0 +1,8 @@
+resource "aws_ami_from_instance" "ami" {
+  depends_on         = [null_resource.app-deploy]
+  name               = "${var.COMPONENT}-${var.APP_VERSION}"
+  source_instance_id = aws_instance.ami.id
+  tags = {
+    Name = "${var.COMPONENT}-${var.APP_VERSION}"
+  }
+}
